4282ad1a2605e8849b6b63c8b46739601409691e
graywa <74241140+graywa@users.noreply.github.com>
2022-01-31 17:07:27 +0300

    theme toggle, header

---
+++ src/components/Header.tsx
@@ -0,0 +1,74 @@
+import React, { FC, useEffect, useState } from 'react'
+import moon from '../accets/img/moon.svg'
+import moonOut from '../accets/img/moon-outline.svg'
+import styled from 'styled-components'
+import { Container } from './Container'
+
+const HeaderWrapper = styled.div`
+  background-color: var(--colors-ui);
+  box-shadow: var(--shadow);
+`
+const HeaderContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 2rem 0;
+`
+
+const HeaderTitle = styled.a`
+
+  transition: all .5s linear; 
+ 
+  &:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }  
+`
+
+const HeaderTogleTheme = styled.div`
+  display: flex;
+  align-items: center;
+  transition: all .5s linear;
+
+  text-transform: capitalize;
+  &:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`
+
+const ThemeIcon = styled.img` 
+  margin-right: .5rem; 
+`
+
+const Header: FC = () => {
+  const [theme, setTheme] = useState('light')
+
+  useEffect(() => {    
+    document.body.setAttribute('data-theme', theme)
+  }, [theme])
+
+  const toggleHandler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
+  
+  return (
+    <HeaderWrapper>
+      <Container>
+        <HeaderContent>
+          <HeaderTitle>
+            Where is the world?
+          </HeaderTitle>
+          <HeaderTogleTheme onClick={toggleHandler}>
+            <ThemeIcon width={20} src={theme === 'light' ? moonOut : moon} alt="theme" />
+            {theme} Theme
+          </HeaderTogleTheme>
+        </HeaderContent>        
+      </Container>
+      
+    </HeaderWrapper>
+  )
+}
+
+export default Header
